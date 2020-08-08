80a9b791e30b976dde2e8890ed6e1c03a8511c80
cyc <1773524112@qq.com>
2020-08-08 21:10:14 +0800

    曹奕昌：增加了部门、员工和爱好的各个组件

---
+++ ruigou/src/components/behave/add.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- Default box -->
+  <div class="box">
+	<div class="box-header with-border">
+	  <h3 class="box-title">增加部门</h3>
+
+	</div>
+	<div class="box-body">
+		  <form action="add.mvc" method="post">
+			  <div class="form-group">
+				<label for="exampleInputEmail1">部门编码</label>
+				<input type="text" class="form-control" name="code" >
+				<small id="emailHelp" class="form-text text-muted"></small>
+			  </div>
+			  <div class="form-group">
+				<label for="exampleInputPsassword1">部门的名称</label>
+				<input type="text" class="form-control" name="name">
+			  </div>
+			  <button type="text" class="btn btn-primary">提交</button>
+		  </form>
+	</div>
+	<!-- /.box-body -->
+	
+  </div>
+  <!-- /.box -->
+</template>
+
+<script>
+	export default{
+		name:"BehaveAdd",
+		data:function(){
+			return {};
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ ruigou/src/components/common/HomeLeft.vue
@@ -0,0 +1,77 @@
+<template>
+	<div>
+		<!-- Left side column. contains the sidebar -->
+		<aside class="main-sidebar">
+		  <!-- sidebar: style can be found in sidebar.less -->
+		  <section class="sidebar">
+		    <!-- Sidebar user panel -->
+		    <div class="user-panel">
+		      <div class="pull-left image">
+		        <img src="./assets/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+		      </div>
+		      <div class="pull-left info">
+		        <p>凯斯柯达</p>
+		        <a href="#"><i class="fa fa-circle text-success"></i> 注销</a>
+		      </div>
+		    </div>
+		    <!-- sidebar menu: : style can be found in sidebar.less -->
+		    <ul class="sidebar-menu" data-widget="tree">
+		      <li class="header">系统功能选择</li>
+		      <li class="treeview">
+		        <a href="#">
+		          <i class="fa fa-dashboard"></i> <span>首页</span>
+		          <span class="pull-right-container">
+		            <i class="fa fa-angle-left pull-right"></i>
+		          </span>
+		        </a>
+		      </li>
+		      <li class="treeview">
+		        <a href="#">
+		          <i class="fa fa-dashboard"></i> <span>收藏商品</span>
+		          <span class="pull-right-container">
+		            <i class="fa fa-angle-left pull-right"></i>
+		          </span>
+		        </a>
+		      </li>
+		      <li class="treeview">
+		        <a href="#">
+		          <i class="fa fa-dashboard"></i> <span>购物车</span>
+		          <span class="pull-right-container">
+		            <i class="fa fa-angle-left pull-right"></i>
+		          </span>
+		        </a>
+		      </li>
+		      <li class="treeview">
+		        <a href="#">
+		          <i class="fa fa-dashboard"></i> <span>订单</span>
+		          <span class="pull-right-container">
+		            <i class="fa fa-angle-left pull-right"></i>
+		          </span>
+		        </a>
+		      </li>
+		      <li class="treeview">
+		        <a href="#">
+		          <i class="fa fa-dashboard"></i> <span>物流</span>
+		          <span class="pull-right-container">
+		            <i class="fa fa-angle-left pull-right"></i>
+		          </span>
+		        </a>
+		      </li>
+		    </ul>
+		  </section>
+		  <!-- /.sidebar -->
+		</aside>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:"HomeLeft",
+		data:function(){
+			return {};
+		}
+	}
+</script>
+
+<style>
+</style>

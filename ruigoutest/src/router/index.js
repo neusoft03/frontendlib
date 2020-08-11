6de1d746b756eb1fba53cc923ae44b7c270019de
cyc <1773524112@qq.com>
2020-08-11 10:45:36 +0800

    曹奕昌：增加了路由守卫

--- ruigoutest/src/router/index.js
+++ ruigoutest/src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+//引入Vuex Store
+import store from './../store/index'
 
 //引入路由管理的组件
 
@@ -68,6 +70,20 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes:routes
+})
+//路由守护实现登录拦截
+router.beforeEach((to,from, next) => {
+	if(to.path=="/login"){
+		next();
+	}
+	else{
+		 if(store.getters.loginuser!=null){
+			 next();
+		 }
+		 else{
+			 next("/login");
+		 }
+	}
 })
 
 export default router
